d31996fec6b9cc8e61e27291134bdee92386403a
pratiksha <pratikshapatil@gmail.com>
2024-10-29 17:38:08 +0530

    "first commit email sending "

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 2222;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
